b571d2551521c97d9a20c3de88e775332e4902b6
Kyle Moore <kylemoore@Kyles-iMac.local>
2019-01-12 14:51:26 +0200

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,24 @@
 class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
-        
     end
+
+    def new
+        @portfolio_new_item = Portfolio.new
+    end
+
+    def create
+        @portfoilio_create = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfoilio_create.save
+            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+            format.json { render :show, status: :created, location: @portfoilio_create }
+          else
+            format.html { render :new }
+          end
+        end
+      end
 
 
 
