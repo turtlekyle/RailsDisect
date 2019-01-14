07ff8e9dcaf2950c474ceedf5a2c9014ac9449f9
Kyle Moore <kylemoore@Kyles-iMac.local>
2019-01-14 11:19:24 +0200

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,3 +49,9 @@ puts "5 skills created"
 
     puts "9 portfolio items created"
     
+    3.times do |technology|
+        Portfolio.last.technologies.create!(
+          name: "Technology #{technology}"
+        )
+    end
+        puts "3 technologies created"
